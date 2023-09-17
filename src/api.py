562fe4874acb59e9c025853a27191e0432635a82
Tor4ubas <alex1029@yandex.ru>
2023-09-17 18:05:43 +0300

    Создан абстрактный класс для работы с API сайта hh.ru с вакансиями

---
+++ src/api.py
@@ -0,0 +1,30 @@
+class WorkApi(ABC):
+    """Абстрактный класс для работы с API"""
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_info(self):
+        pass
+
+
+class HeadHunter(WorkApi):
+    """Класс для работы с API HeadHunter"""
+
+    url = 'https://api.hh.ru/vacancies'
+
+    def __init__(self, text: str, per_page: int):
+        self.text = text
+        self.per_page = per_page
+
+
+    def get_info(self):
+        """
+        Получает список вакансий
+        :return: list
+        """
+        response = requests.get(self.url, params=self.__dict__)
+        info = response.json()['items']
+        return info
