2a018a7f0b4fc00797ff71ca8b3b8163ff576a3d
Tor4ubas <alex1029@yandex.ru>
2023-09-19 09:24:14 +0300

    Создан абстрактный класс для работы с API сайта SJ.ru с вакансиями

--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import requests as requests
+import os
 
 class WorkApi(ABC):
     """Абстрактный класс для работы с API"""
@@ -30,4 +31,24 @@ class HeadHunter(WorkApi):
         """
         response = requests.get(self.url, params=self.__dict__)
         info = response.json()['items']
+        return info
+
+class SuperJob(WorkApi):
+    """Класс для работы с API SuperJob"""
+
+    API_KEY = {'X-Api-App-Id': os.getenv('SUPERJOB_API_KEY')}
+    url = 'https://api.superjob.ru/2.0/vacancies/'
+
+    def __init__(self, text: str, t=None, c=None):
+        self.keyword = text
+        self.t = t
+        self.c = c
+
+    def get_info(self):
+        """
+        Получает список вакансий
+        :return: list
+        """
+        response = requests.get(self.url, headers=self.API_KEY, params=self.__dict__)
+        info = response.json()['objects']
         return info
